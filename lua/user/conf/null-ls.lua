e9871e2b21387cbe90f6ba073e23a14b23d86cae
acoolda <yezhipeng@wjacloud.com>
2023-04-21 14:32:14 +0800

    修复bufferline在linux下报错，添加项目pattern

--- lua/user/conf/null-ls.lua
+++ lua/user/conf/null-ls.lua
@@ -1,10 +1,10 @@
-local null_ls_status,null_ls = pcall(require,"null-ls")
+local null_ls_status, null_ls = pcall(require, "null-ls")
 if not null_ls_status then
   return
 end
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#formatting
 local formatting = null_ls.builtins.formatting
--- 
+--
 local diagnostics = null_ls.builtins.diagnostics
 
 local codeaction = null_ls.builtins.code_actions
@@ -13,10 +13,10 @@ null_ls.setup({
   debug = false,
   sources = {
     -- go formating
-    formatting.goimports.with({extra_args = {"-srcdir","$DIRNAME"}}),
+    formatting.goimports.with({ extra_args = { "-srcdir", "$DIRNAME" } }),
     -- go diagnostics
     -- 根据官方参数配置添加了run有问题，去掉就正常了
-    diagnostics.golangci_lint.with({extra_args={ "--fix=false", "--out-format=json", "--path-prefix", "$ROOT"}}),
+    diagnostics.golangci_lint,
     -- go code action
     codeaction.gomodifytags,
   }
