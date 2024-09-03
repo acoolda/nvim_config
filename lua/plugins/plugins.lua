26fa484eec0816e7b6080bcd193531b6f9a6f3dc
acoolda <acoolda@qq.com>
2024-09-03 10:23:13 +0800

    feat：新增了db访问插件

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -783,4 +783,30 @@ return {
       "rcarriga/nvim-notify",
     }
   },
+  {
+    "kndndrj/nvim-dbee",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    build = function()
+      -- Install tries to automatically detect the install method.
+      -- if it fails, try calling it with one of these parameters:
+      --    "curl", "wget", "bitsadmin", "go"
+      require("dbee").install("go")
+    end,
+    config = function()
+      require("dbee").setup { --[[optional config]]
+        sources = {
+          require("dbee.sources").MemorySource:new(
+            {
+              name = "local-dev-5.7",
+              type = "mysql",
+              url = "mysql://root:root@localhost:3306/gitee_development"
+            }
+          )
+        }
+
+      }
+    end,
+  },
 }
